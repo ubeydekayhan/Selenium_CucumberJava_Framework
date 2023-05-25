6eab708eb5a2474eae898105ff234fb7c9e3701d
Mustafa Ubeyde Kayhan <mubeydekayhan.42@gmail.com>
2023-05-25 17:46:57 +0300

    mustafa calisma

---
+++ src/test/java/team12/runner/Runner.java
@@ -0,0 +1,26 @@
+package team12.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(plugin = {"pretty",
+        "html:src/test/resources/features/htmlReport/cucumberHooks.html",
+        "json:target/json-reports/cucumber.json",
+        "junit:src/test/resources/features/htmlReport/cucumber.xml",
+        "rerun:TestOutput/failed_scenario.txt",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+
+        features = "src/test/resources/features",
+        glue = {"techproed/stepDefinitions"},
+        tags = "@techpro1",
+        dryRun = false,
+
+        monochrome = true
+)
+
+ public class Runner {
+}
